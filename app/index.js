a45c9c40161badde08a3669c104fee9d60176b51
JustRoniss <ronaldoluizdev@gmail.com>
2024-01-24 09:54:45 -0300

    Script inicial de criação de tabela + inserção no banco de dados

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
@@ -10,13 +10,41 @@ const configDb = {
     database: 'db_desafio'
 };
 
-const connection = mysql.createConnection(configDb);
-const query = `INSERT INTO people(name) values('Ronaldo');`;
-connection.query(query);
-connection.end();
+const pool = mysql.createPool(configDb);
+
+const queryInsert = `INSERT INTO people(name) values('Ronaldo');`;
+const querySelect = `SELECT * FROM people;`;
+
 
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle - Docker: Desafio 2</h1>');
+
+
+    pool.query(queryInsert, (err, results, fields) => {
+        if(err){
+            console.error("Erro ao inserir registro");
+        }
+        console.log('Registro inserido com sucesso: ', results.insertId);
+    })
+
+    pool.query(querySelect, (err, results) => {
+        if(err){
+            console.error("Erro ao consultar tabela");
+            res.send("Erro ao consultar tabela");
+            return;
+        }
+
+        let html = '<h1>Full Cycle Rocks!</h1> <br/> <h3>Você pode atualizar a página para criar um novo registro!<h3/> ';
+        html += '<table border="1"><tr><th>ID</th><th>Nome</th></tr>';
+
+        results.forEach(row => {
+            html += `<tr><td>${row.id}</td><td>${row.name}</td></tr>`;
+        });
+
+        html += '</table>';
+ 
+        res.send(html);
+    })
+
 });
 
 app.listen(port, ()=>{
